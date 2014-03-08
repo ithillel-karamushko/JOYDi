6eb639aeaf950f2dc83a0a618e8b20104ff95448
Yulia Brus <aisy.fibi@gmail.com>
2014-03-08 21:20:13 +0200

    Getter and setter for class comment

--- JOYDi/src/org/hillel/it/joydi/model/entities/Comment.java
+++ JOYDi/src/org/hillel/it/joydi/model/entities/Comment.java
@@ -6,8 +6,53 @@ public class Comment {
 	private String authorName;
 	private int likes;
 	private int dislikes;
+    private String commentText;
+    
+	//I dont know if we need this part
+	/*public double getAddingDate() {
+		return addingDate;
+	}*/
 
+	public void setAddingDate(double addingDate) {
+		this.addingDate = addingDate;
+	}
+	//I dont know if we need this part
+	/*public double getAddingTime() {
+		return addingTime;
+	}*/
+
+	public void setAddingTime(double addingTime) {
+		this.addingTime = addingTime;
+	}
+
+    public String getAuthorName(){
+    	return authorName;
+    }
 	public void setAuthorName(String authorName) {
 		this.authorName = authorName;
 	}
+
+
+	public int getLikes() {
+		return likes;
+	}
+	
+	public int getDislikes() {
+		return dislikes;
+	}
+
+	public String getCommentText() {
+		return commentText;
+	}
+
+	public void setCommentText(String commentText) {
+		this.commentText = commentText;
+	}
+
+
+	
+
+	
+	
+	
 }
