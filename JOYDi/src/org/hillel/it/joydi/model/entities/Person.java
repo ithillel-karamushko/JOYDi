00de84639a341d769d1c221c937916600a12dc8d
Karamushko Evgeniy <ecoevgeny@gmail.com>
2014-03-08 20:57:26 +0200

    With get- and set-

--- JOYDi/src/org/hillel/it/joydi/model/entities/Person.java
+++ JOYDi/src/org/hillel/it/joydi/model/entities/Person.java
@@ -8,4 +8,35 @@ abstract public class Person {
 	protected String country;
 	protected String sex;
 	protected int age; 
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
