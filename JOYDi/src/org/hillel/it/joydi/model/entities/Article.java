fdf5fd107f642b85117faf70068c330eb69099a9
Yulia Brus <aisy.fibi@gmail.com>
2014-03-08 21:30:40 +0200

    Getter and setter for class article

--- JOYDi/src/org/hillel/it/joydi/model/entities/Article.java
+++ JOYDi/src/org/hillel/it/joydi/model/entities/Article.java
@@ -6,5 +6,42 @@ public class Article {
 	private String themeOfTheArticle;
 	private double addingDate;
 	private double addingTime;
-    private int ratingOfTheArticle; 
+    private int ratingOfTheArticle;
+	
+    public String getTextOfTheArticle() {
+		return textOfTheArticle;
+	}
+	public void setTextOfTheArticle(String textOfTheArticle) {
+		this.textOfTheArticle = textOfTheArticle;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public String getThemeOfTheArticle() {
+		return themeOfTheArticle;
+	}
+	public void setThemeOfTheArticle(String themeOfTheArticle) {
+		this.themeOfTheArticle = themeOfTheArticle;
+	}
+	public double getAddingDate() {
+		return addingDate;
+	}
+	public void setAddingDate(double addingDate) {
+		this.addingDate = addingDate;
+	}
+	public double getAddingTime() {
+		return addingTime;
+	}
+	public void setAddingTime(double addingTime) {
+		this.addingTime = addingTime;
+	}
+	public int getRatingOfTheArticle() {
+		return ratingOfTheArticle;
+	}
+	public void setRatingOfTheArticle(int ratingOfTheArticle) {
+		this.ratingOfTheArticle = ratingOfTheArticle;
+	} 
 }
