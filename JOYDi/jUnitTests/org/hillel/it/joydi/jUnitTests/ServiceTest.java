e8bfe2b4d6bf7e2bf45cfd50b825054c0626ec50
Karamushko Evgeniy <ecoevgeny@gmail.com>
2014-04-09 12:28:16 +0300

    Correction test

--- JOYDi/jUnitTests/org/hillel/it/joydi/jUnitTests/ServiceTest.java
+++ JOYDi/jUnitTests/org/hillel/it/joydi/jUnitTests/ServiceTest.java
@@ -14,11 +14,19 @@ import org.hillel.it.joydi.service.imp.DiaryServiceImpl;
 import org.junit.Test;
 
 public class ServiceTest {
-	//
-	// @Test
-	// public void testSaveArticle() {
-	// fail("Not yet implemented");
-	// }
+
+	@Test
+	public void testSaveArticle() {
+
+		TextRepository textRepository = new InMemoryTextRepository();
+		Article article = new Article("Hanna", "Java", "Java is bad.");
+		DiaryServiceImpl ds = new DiaryServiceImpl();
+
+		int i;
+		i = textRepository.getArticle().size() + 1;
+		ds.saveArticle(article);
+		assertEquals("Incorrect", i, textRepository.getArticle().size());
+	}
 
 	// @Test
 	// public void testDeleteArticle() {
@@ -79,27 +87,19 @@ public class ServiceTest {
 	// public void testGetTextsList() {
 	// fail("Not yet implemented");
 	// }
-	
-	private TextRepository textRepository = new InMemoryTextRepository();
-	private PersonRepository personRepository = new InMemoryPersonRepository();	
 
-	Article article = new Article("Hanna", "Java", "Java is bad.");
-	DiaryServiceImpl ds = new DiaryServiceImpl();
-	 
-	public void TestSaveArticle(){
-		int i;
-		i = textRepository.getArticle().size()+1;
-		ds.saveArticle(article);
-		assertEquals("Incorrect", i, textRepository.getArticle().size());
-	}
-	
+	@Test
 	public void testPushLike() {
+		Article article = new Article("Hanna", "Java", "Java is bad.");
+		DiaryServiceImpl ds = new DiaryServiceImpl();
 		ds.pushLike(article);
 		assertEquals("Incorrect", 1, article.getLike());
 	}
 
 	@Test
 	public void testPushDisLike() {
+		Article article = new Article("Hanna", "Java", "Java is bad.");
+		DiaryServiceImpl ds = new DiaryServiceImpl();
 		ds.pushDisLike(article);
 		assertEquals("Incorrect", 1, article.getDisLike());
 	}
