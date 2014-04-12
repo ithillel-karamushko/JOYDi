33da21a31d61dcac738c98bf006b278c453b75a6
Olga Grygoryan <Lamzaki_Olga@ukr.net>
2014-04-12 14:09:31 +0300

    + javadoc in classes Article&Comment

--- JOYDi/src/org/hillel/it/joydi/model/entities/Article.java
+++ JOYDi/src/org/hillel/it/joydi/model/entities/Article.java
@@ -3,6 +3,12 @@ package org.hillel.it.joydi.model.entities;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * 
+ * @author Îëÿ
+ *
+ */
+
 public class Article extends BaseEntity {
 	private String textOfTheArticle;
 	private String authorName;
@@ -13,6 +19,18 @@ public class Article extends BaseEntity {
 	private int like;
 	private int disLike;
 
+	/**
+	 * Kit of the class Article takes parameters authorName, themeOfTheArticle,
+	 * and String textOfTheArticle and sets their values in fields authorName, 
+	 * themeOfTheArticle and String textOfTheArticle,
+	 * also it set in field date current date and time and in fields like
+	 * and disLike values = 0
+	 * 
+	 * @param authorName
+	 * @param themeOfTheArticle
+	 * @param textOfTheArticle
+	 */
+	
 	public Article(String authorName, String themeOfTheArticle,
 			String textOfTheArticle) {
 		date = new Date();
@@ -23,66 +41,154 @@ public class Article extends BaseEntity {
 		this.setDisLike(0);
 	}
 
+	/**
+	 * Getter for date
+	 * @return field date
+	 */
+	
 	public Date getDate() {
 		return date;
 	}
 
+	/**
+	 * Setter for date
+	 * modify field date
+	 * @param date
+	 */
+	
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
+	/**
+	 * Getter for tags
+	 * @return field tags
+	 */
+	
 	public List getTags() {
 		return tags;
 	}
 
+	/**
+	 * Setter for tags
+	 * modify field tags
+	 * @param tags
+	 */
+	
 	public void setTags(List tags) {
 		this.tags = tags;
 	}
 
+	/**
+	 * Getter for textOfTheArticle
+	 * @return field textOfTheArticle
+	 */
+	
 	public String getTextOfTheArticle() {
 		return textOfTheArticle;
 	}
 
+	/**
+	 * Setter for textOfTheArticle
+	 * modify field textOfTheArticle
+	 * @param textOfTheArticle
+	 */
+	
 	public void setTextOfTheArticle(String textOfTheArticle) {
 		this.textOfTheArticle = textOfTheArticle;
 	}
 
+	/**
+	 * Getter for authorName
+	 * @return field authorName
+	 */
+	
 	public String getAuthorName() {
 		return authorName;
 	}
 
+	/**
+	 * Setter for setAuthorName
+	 * modify field setAuthorName
+	 * @param authorName
+	 */
+	
 	public void setAuthorName(String authorName) {
 		this.authorName = authorName;
 	}
 
+	/**
+	 * Getter for themeOfArticle
+	 * @return field themeOfArticle
+	 */
+	
 	public String getThemeOfTheArticle() {
 		return themeOfTheArticle;
 	}
 
+	/**
+	 * Setter of themeOfArticle
+	 * modify field  themeOfArfticle
+	 * @param themeOfTheArticle
+	 */
+	
 	public void setThemeOfTheArticle(String themeOfTheArticle) {
 		this.themeOfTheArticle = themeOfTheArticle;
 	}
 
+	/**
+	 * Getter for ratingOfArticle
+	 * @return field ratingOfArticle
+	 */
+	
 	public int getRatingOfTheArticle() {
 		return ratingOfTheArticle;
 	}
 
+	/**
+	 * Setter for ratingOfArticle
+	 * modify field ratingOfArticle 
+	 * @param ratingOfTheArticle
+	 */
+	
 	public void setRatingOfTheArticle(int ratingOfTheArticle) {
 		this.ratingOfTheArticle = ratingOfTheArticle;
 	}
 
+	/**
+	 * Getter for like
+	 * @return field like
+	 */
+	
 	public int getLike() {
 		return like;
 	}
 
+	/**
+	 * Setter for like
+	 * modify field like
+	 * @param like
+	 */
+	
 	public void setLike(int like) {
 		this.like = like;
 	}
+	
+	/**
+	 * Getter for disLike 
+	 * @return field disLike
+	 */
 	
 	public int getDisLike() {
 		return disLike;
 	}
 
+	/**
+	 * Setter for disLike
+	 * modify field disLike
+	 * @param disLike
+	 */
+	
 	public void setDisLike(int disLike) {
 		this.disLike = disLike;
 	}
--- JOYDi/src/org/hillel/it/joydi/model/entities/Comment.java
+++ JOYDi/src/org/hillel/it/joydi/model/entities/Comment.java
@@ -3,14 +3,27 @@ package org.hillel.it.joydi.model.entities;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * 
+ * @author Îëÿ
+ * 
+ */
+
 public class Comment extends BaseEntity {
-	// private Date addingDate = new Date();
-	// private double addingTime;
 	private String authorName;
 	private int likes;
 	private int dislikes;
 	private String commentText;
 	Date date;
+
+	/**
+	 * Kit of the class Comment takes parameters authorName and commentText 
+	 * and sets their values in fields authorName and commentText
+	 * also it set in field date current date and time
+	 * 
+	 * @param authorName
+	 * @param commentText
+	 */
 
 	public Comment(String authorName, String commentText) {
 		this.authorName = authorName;
@@ -19,6 +32,11 @@ public class Comment extends BaseEntity {
 
 	}
 
+	/**
+	 * Getter for field Date
+	 * @return Date
+	 */
+	
 	public Date getDate() {
 		return date;
 	}
@@ -26,27 +44,59 @@ public class Comment extends BaseEntity {
 	SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy hh:mm");
 
 	// I don`t know if we need this part
-
+	
+	/**
+	 *  Getter for field AuthorName
+	 * @return field authorName
+	 */
+	
 	public String getAuthorName() {
 		return authorName;
 	}
 
+	/**
+	 * Setter for authorName
+	 * modify authorName
+	 * @param authorName
+	 */
+	
 	public void setAuthorName(String authorName) {
 		this.authorName = authorName;
 	}
 
+	/**
+	 * Getter for likes
+	 * @return field likes
+	 */
+	
 	public int getLikes() {
 		return likes;
 	}
 
+	/**
+	 * Getter for dislikes
+	 * @return dislikes
+	 */
+	
 	public int getDislikes() {
 		return dislikes;
 	}
 
+	/**
+	 * Getter for commentText
+	 * @return commentText
+	 */
+	
 	public String getCommentText() {
 		return commentText;
 	}
 
+	/**
+	 * Setter for commentText
+	 * modify commentText
+	 * @param commentText
+	 */
+	
 	public void setCommentText(String commentText) {
 		this.commentText = commentText;
 	}
